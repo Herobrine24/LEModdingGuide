3efdaa0e91c9a4a75f98e99f518e8bf92fa277ce
Herobrine24 <73685731+Herobrine24@users.noreply.github.com>
2023-03-18 17:41:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,101 @@
-const listContainer = document.getElementById("list");
-const filterButtons = document.getElementById("filter-buttons").getElementsByTagName("button");
+const filterContainer = document.querySelector('.filter');
+const itemList = document.querySelector('.item-list');
 
-// Load the JSON file
-fetch("items.json")
+function createListItem(item) {
+  const listItem = document.createElement('li');
+  listItem.className = `item ${item.filter} show`;
+  const link = document.createElement('a');
+  link.href = item.link;
+  link.style.color = 'blue';
+  link.style.display = 'inline';
+  link.setAttribute('target', '_blank'); // Add this line to set the target attribute
+  const linkText = document.createTextNode(item.name);
+  link.appendChild(linkText);
+  listItem.appendChild(link);
+  const separator = document.createElement('strong');
+  separator.appendChild(document.createTextNode(' - '));
+  listItem.appendChild(separator);
+  const description = document.createElement('span');
+  description.style.display = 'inline-flex';
+  description.style.flexDirection = 'column';
+  const descriptionText = document.createTextNode(item.description);
+  description.appendChild(descriptionText);
+  listItem.appendChild(description);
+  return listItem;
+}
+
+// Load data from Filter.json
+fetch('Filter.json')
   .then(response => response.json())
   .then(data => {
-    // Create a list item for each item in the JSON file
-    data.forEach(item => {
-      const listItem = document.createElement("li");
-      listItem.innerHTML = item.text;
-      listItem.dataset.category = item.category;
-      listContainer.appendChild(listItem);
+    // Store the data in variables
+    const items = data.items;
+    const filters = data.filters;
+
+    // Add filter buttons
+    filters.forEach((filter) => {
+      const button = document.createElement('button');
+      button.className = 'filter-button';
+      button.dataset.filter = filter.filter;
+      button.textContent = filter.name;
+      filterContainer.appendChild(button);
     });
 
-    // Set up event listeners for the filter buttons
-    for (const button of filterButtons) {
+    const filterButtons = document.querySelectorAll(".filter-button");
+
+    // Add active class to All filter button by default
+    filterButtons.forEach((button) => {
+      if (button.dataset.filter === "all") {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+
+    // Show all items on page load
+    items.forEach((item) => {
+      const listItem = createListItem(item);
+      itemList.appendChild(listItem);
+    });
+
+    // Add click event listener to each filter button
+    filterButtons.forEach((button) => {
       button.addEventListener("click", () => {
         const filter = button.dataset.filter;
 
-        // Show or hide list items based on the filter
-        for (const listItem of listContainer.getElementsByTagName("li")) {
-          if (filter === "all" || listItem.dataset.category === filter) {
-            listItem.style.display = "block";
+        // Remove active class from all filter buttons
+        filterButtons.forEach((button) => {
+          button.classList.remove("active");
+        });
+
+        // Add active class to clicked filter button
+        button.classList.add("active");
+
+        // Show or hide items based on filter
+        items.forEach((item) => {
+          const listItem = itemList.querySelector(`.item.${item.filter}`);
+          if (filter === "all") {
+            listItem.classList.add("show");
+          } else if (item.filter === filter) {
+            listItem.classList.add("show");
           } else {
-            listItem.style.display = "none";
+            listItem.classList.remove("show");
           }
+        });
+
+        // Hide items not matching the filter
+        const hiddenItems = document.querySelectorAll(`.item:not(.${filter})`);
+        hiddenItems.forEach((item) => {
+          item.classList.remove("show");
+        });
+
+        // Show all items on clicking the "All" filter
+        if (filter === "all") {
+          const allItems = document.querySelectorAll(".item");
+          allItems.forEach((item) => {
+            item.classList.add("show");
+          });
         }
       });
-    }
-  })
-  .catch(error => console.error(error));
+    });
+  });
