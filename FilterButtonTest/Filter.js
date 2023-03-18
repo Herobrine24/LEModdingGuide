1bb0ac8ac380371276116c8c775cb1d3d6f18238
Herobrine24 <73685731+Herobrine24@users.noreply.github.com>
2023-03-18 12:45:39 +0100

    Update Filter.js

--- FilterButtonTest/Filter.js
+++ FilterButtonTest/Filter.js
@@ -1,67 +1,23 @@
-const filterButtons = document.querySelectorAll(".filter-button");
-const itemList = document.querySelector('.item-list');
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.dataset.filter;
 
-// Load data from Filter.json
-fetch('Filter.json')
-  .then(response => response.json())
-  .then(data => {
-    // Store the data in an array
-    const items = data.items;
-
-    // Add active class to All filter button by default
+    // Remove active class from all filter buttons
     filterButtons.forEach((button) => {
-      if (button.dataset.filter === "all") {
-        button.classList.add("active");
-      } else {
-        button.classList.remove("active");
-      }
+      button.classList.remove("active");
     });
 
-    // Show all items on page load
-    items.forEach((item) => {
-      const listItem = document.createElement('li');
-      listItem.className = `item ${item.filter} show`;
-      const link = document.createElement('a');
-      link.href = item.link;
-      link.style.color = 'blue';
-      link.style.display = 'inline';
-      const linkText = document.createTextNode(item.name);
-      link.appendChild(linkText);
-      listItem.appendChild(link);
-      const separator = document.createElement('strong');
-      separator.appendChild(document.createTextNode(' - '));
-      listItem.appendChild(separator);
-      const description = document.createElement('span');
-      description.style.display = 'inline-flex';
-      description.style.flexDirection = 'column';
-      const descriptionText = document.createTextNode(item.description);
-      description.appendChild(descriptionText);
-      listItem.appendChild(description);
-      itemList.appendChild(listItem);
+    // Add active class to clicked filter button
+    button.classList.add("active");
+
+    // Show or hide items based on filter
+    const itemsToShow = itemList.querySelectorAll(".item." + filter);
+    const itemsToHide = itemList.querySelectorAll(".item:not(." + filter + ")");
+    itemsToShow.forEach((item) => {
+      item.classList.add("show");
     });
-
-    // Add click event listener to each filter button
-    filterButtons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const filter = button.dataset.filter;
-
-        // Remove active class from all filter buttons
-        filterButtons.forEach((button) => {
-          button.classList.remove("active");
-        });
-
-        // Add active class to clicked filter button
-        button.classList.add("active");
-
-        // Show or hide items based on filter
-        items.forEach((item) => {
-          const listItem = itemList.querySelector(`.item.${item.filter}`);
-          if (filter === "all" || item.filter === filter) {
-            listItem.classList.add("show");
-          } else {
-            listItem.classList.remove("show");
-          }
-        });
-      });
+    itemsToHide.forEach((item) => {
+      item.classList.remove("show");
     });
   });
+});
