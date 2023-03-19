e63536638693856a212399aaf4963d1d58dd170a
Herobrine24 <73685731+Herobrine24@users.noreply.github.com>
2023-03-19 09:39:46 +0100

    Update DoubleFilter.js

--- DoubleFilter/DoubleFilter.js
+++ DoubleFilter/DoubleFilter.js
@@ -1,61 +1,63 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const gameFilter = document.querySelectorAll('.gameFilter');
-  const modFilter = document.querySelectorAll('.modFilter');
-  const modList = document.querySelector('.modList');
-  const defaultGameFilter = 'all';
-  const defaultModFilter = 'all';
+const gameFilter = document.querySelectorAll('.gameFilter');
+const modFilter = document.querySelectorAll('.modFilter');
+const modList = document.querySelector('.modList');
+const defaultGameFilter = 'all';
+const defaultModFilter = 'all';
 
-  fetch('DoubleFilter.json')
-    .then(response => response.json())
-    .then(modData => {
-      let filteredMods = modData.filter(mod => mod.game === defaultGameFilter);
+fetch('DoubleFilter.json')
+  .then(response => response.json())
+  .then(modData => {
+    if (!Array.isArray(modData)) {
+      throw new TypeError('modData is not an array');
+    }
 
-      renderModList(filteredMods);
+    let filteredMods = modData.filter(mod => mod.game === defaultGameFilter);
 
-      gameFilter.forEach(filter => {
-        filter.addEventListener('click', () => {
-          const selectedFilter = filter.dataset.game;
+    renderModList(filteredMods);
 
-          filteredMods = modData.filter(mod => mod.game === selectedFilter);
+    gameFilter.forEach(filter => {
+      filter.addEventListener('click', () => {
+        const selectedFilter = filter.dataset.game;
 
-          renderModList(filteredMods);
-        });
+        filteredMods = modData.filter(mod => mod.game === selectedFilter);
+
+        renderModList(filteredMods);
       });
-
-      modFilter.forEach(filter => {
-        filter.addEventListener('click', () => {
-          const selectedFilter = filter.dataset.mod;
-
-          if (selectedFilter === defaultModFilter) {
-            filteredMods = modData.filter(mod => mod.game === defaultGameFilter);
-          } else {
-            filteredMods = modData.filter(mod => mod.type === selectedFilter && mod.game === defaultGameFilter);
-          }
-
-          renderModList(filteredMods);
-        });
-      });
-    })
-    .catch(error => console.log('Error:', error));
-
-  function renderModList(mods) {
-    modList.innerHTML = '';
-
-    mods.forEach(mod => {
-      const modListItem = document.createElement('li');
-
-      const modLink = document.createElement('a');
-      modLink.href = mod.link;
-      modLink.target = '_blank';
-      modLink.textContent = mod.name;
-
-      const modDescription = document.createElement('span');
-      modDescription.textContent = ` - ${mod.description}`;
-
-      modListItem.appendChild(modLink);
-      modListItem.appendChild(modDescription);
-
-      modList.appendChild(modListItem);
     });
-  }
-});
+
+    modFilter.forEach(filter => {
+      filter.addEventListener('click', () => {
+        const selectedFilter = filter.dataset.mod;
+
+        if (selectedFilter === defaultModFilter) {
+          filteredMods = modData.filter(mod => mod.game === defaultGameFilter);
+        } else {
+          filteredMods = modData.filter(mod => mod.type === selectedFilter && mod.game === defaultGameFilter);
+        }
+
+        renderModList(filteredMods);
+      });
+    });
+  })
+  .catch(error => console.log('Error:', error));
+
+function renderModList(mods) {
+  modList.innerHTML = '';
+
+  mods.forEach(mod => {
+    const modListItem = document.createElement('li');
+
+    const modLink = document.createElement('a');
+    modLink.href = mod.link;
+    modLink.target = '_blank';
+    modLink.textContent = mod.name;
+
+    const modDescription = document.createElement('span');
+    modDescription.textContent = ` - ${mod.description}`;
+
+    modListItem.appendChild(modLink);
+    modListItem.appendChild(modDescription);
+
+    modList.appendChild(modListItem);
+  });
+}
