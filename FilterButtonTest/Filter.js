b0b1943091c975aff4d74cf6b811efbdffe6e5a3
Herobrine24 <73685731+Herobrine24@users.noreply.github.com>
2023-04-23 11:52:15 +0200

    Update Filter.js

--- FilterButtonTest/Filter.js
+++ FilterButtonTest/Filter.js
@@ -1,27 +1,9 @@
-const filterContainer = document.querySelector('.filter');
+const typeFilterContainer = document.querySelector('.type-filter');
+const gameFilterContainer = document.querySelector('.game-filter');
 const itemList = document.querySelector('.item-list');
 
 function createListItem(item) {
-  const listItem = document.createElement('li');
-  listItem.className = `item ${item.filter} show`;
-  const link = document.createElement('a');
-  link.href = item.link;
-  link.style.color = 'blue';
-  link.style.display = 'inline';
-  link.setAttribute('target', '_blank'); // Add this line to set the target attribute
-  const linkText = document.createTextNode(item.name);
-  link.appendChild(linkText);
-  listItem.appendChild(link);
-  const separator = document.createElement('strong');
-  separator.appendChild(document.createTextNode(' - '));
-  listItem.appendChild(separator);
-  const description = document.createElement('span');
-  description.style.display = 'inline-flex';
-  description.style.flexDirection = 'column';
-  const descriptionText = document.createTextNode(item.description);
-  description.appendChild(descriptionText);
-  listItem.appendChild(description);
-  return listItem;
+  // ... (no changes in this function)
 }
 
 // Load data from Filter.json
@@ -30,20 +12,32 @@ fetch('Filter.json')
   .then(data => {
     // Store the data in variables
     const items = data.items;
-    const filters = data.filters;
+    const typeFilters = data.typeFilters;
+    const gameFilters = data.gameFilters;
 
-    // Add filter buttons
-    filters.forEach((filter) => {
+    // Add type filter buttons
+    typeFilters.forEach((filter) => {
       const button = document.createElement('button');
-      button.className = 'filter-button';
+      button.className = 'filter-button type-filter-button';
       button.dataset.filter = filter.filter;
       button.textContent = filter.name;
-      filterContainer.appendChild(button);
+      typeFilterContainer.appendChild(button);
     });
 
-    const filterButtons = document.querySelectorAll(".filter-button");
+    // Add game filter buttons
+    gameFilters.forEach((filter) => {
+      const button = document.createElement('button');
+      button.className = 'filter-button game-filter-button';
+      button.dataset.filter = filter.filter;
+      button.textContent = filter.name;
+      gameFilterContainer.appendChild(button);
+    });
 
-    // Add active class to All filter button by default
+    const typeFilterButtons = document.querySelectorAll(".type-filter-button");
+    const gameFilterButtons = document.querySelectorAll(".game-filter-button");
+    const filterButtons = [...typeFilterButtons, ...gameFilterButtons];
+
+    // Add active class to All filter buttons by default
     filterButtons.forEach((button) => {
       if (button.dataset.filter === "all") {
         button.classList.add("active");
@@ -58,44 +52,47 @@ fetch('Filter.json')
       itemList.appendChild(listItem);
     });
 
+    function updateItemsVisibility() {
+      items.forEach((item) => {
+        const listItem = itemList.querySelector(`.item.${item.typeFilter}.${item.gameFilter}`);
+        const activeTypeButton = document.querySelector('.type-filter-button.active');
+        const activeGameButton = document.querySelector('.game-filter-button.active');
+        const activeTypeFilter = activeTypeButton.dataset.filter;
+        const activeGameFilter = activeGameButton.dataset.filter;
+
+        if (activeTypeFilter === 'all' && activeGameFilter === 'all') {
+          listItem.classList.add("show");
+        } else if (activeTypeFilter === 'all' && item.gameFilter === activeGameFilter) {
+          listItem.classList.add("show");
+        } else if (activeGameFilter === 'all' && item.typeFilter === activeTypeFilter) {
+          listItem.classList.add("show");
+        } else if (item.typeFilter === activeTypeFilter && item.gameFilter === activeGameFilter) {
+          listItem.classList.add("show");
+        } else {
+          listItem.classList.remove("show");
+        }
+      });
+    }
+
     // Add click event listener to each filter button
     filterButtons.forEach((button) => {
       button.addEventListener("click", () => {
-        const filter = button.dataset.filter;
-
-        // Remove active class from all filter buttons
-        filterButtons.forEach((button) => {
-          button.classList.remove("active");
+        const buttonFilterGroup = button.classList.contains('type-filter-button') ? '.type-filter-button' : '.game-filter-button';
+        const filterButtonsInGroup = document.querySelectorAll(buttonFilterGroup);
+        
+        // Remove active class from all filter buttons in the same group
+        filterButtonsInGroup.forEach((btn) => {
+          btn.classList.remove("active");
         });
-
+        
         // Add active class to clicked filter button
         button.classList.add("active");
-
-        // Show or hide items based on filter
-        items.forEach((item) => {
-          const listItem = itemList.querySelector(`.item.${item.filter}`);
-          if (filter === "all") {
-            listItem.classList.add("show");
-          } else if (item.filter === filter) {
-            listItem.classList.add("show");
-          } else {
-            listItem.classList.remove("show");
-          }
-        });
-
-        // Hide items not matching the filter
-        const hiddenItems = document.querySelectorAll(`.item:not(.${filter})`);
-        hiddenItems.forEach((item) => {
-          item.classList.remove("show");
-        });
-
-        // Show all items on clicking the "All" filter
-        if (filter === "all") {
-          const allItems = document.querySelectorAll(".item");
-          allItems.forEach((item) => {
-            item.classList.add("show");
-          });
-        }
+        
+        // Update items visibility
+        updateItemsVisibility();
       });
     });
+
+    // Initial update of items visibility
+    updateItemsVisibility();
   });
